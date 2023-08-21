7d76c894eb6f302321df7c07fb895e539672dc75
felipricardo <felipebarbosaok@gmail.com>
2023-08-21 10:40:24 -0300

    up

--- CamadaDados/DCategoria.cs
+++ CamadaDados/DCategoria.cs
@@ -3,10 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace CamadaDados
 {
     internal class DCategoria
     {
+        private int _Idcategoria;
+        private string _Nome;
+        private string _Descricao;
+        private string _TextoBuscar;
+
+        public int Idcategoria { get => _Idcategoria; set => _Idcategoria = value; }
+        public string Nome { get => _Nome; set => _Nome = value; }
+        public string Descricao { get => _Descricao; set => _Descricao = value; }
+        public string TextoBuscar { get => _TextoBuscar; set => _TextoBuscar = value; }
+
+        // Construtor vazio
+
+        public DCategoria()
+        {
+
+        }
+
+        // Construtor com Parametros
+        public DCategoria(int idcategoria, string nome, string descricao, string textobuscar)
+        {
+            this.Idcategoria = idcategoria;
+            this.Nome = nome;
+            this.Descricao = descricao;
+            this.TextoBuscar = textobuscar;
+        }
+
+        // Método Inserir
+        public string Inserir(DCategoria Categoria)
+        {
+
+        }
+
+        // Método Editar
+        public string Editar(DCategoria Categoria)
+        {
+
+        }
+
+        // Método Excluir
+        public string Excluir(DCategoria Categoria)
+        {
+
+        }
+
+        // Método Mostrar
+        public DataTable Mostrar(DCategoria Categoria)
+        {
+
+        }
     }
 }
