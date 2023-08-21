f4f52d2e89d27afe14dd43b8a23fd1dc3943dcdd
felipricardo <felipebarbosaok@gmail.com>
2023-08-21 09:31:57 -0300

    up

--- CamadaDados/Conexao.cs
+++ CamadaDados/Conexao.cs
@@ -6,8 +6,8 @@ using System.Threading.Tasks;
 
 namespace CamadaDados
 {
-    public class Class1
+    internal class Conexao
     {
-
+        public static string Cn = "Data source = DESKTOP-B8GQSQB; Initial Catalog = dbcomercio; Integrated Security=true";
     }
 }
