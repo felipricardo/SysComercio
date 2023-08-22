52503cfb11e32e53cb4450df8aa3767e415f4fa1
felipricardo <felipebarbosaok@gmail.com>
2023-08-22 10:23:51 -0300

    up

--- CamadadeApresentacao/frmCategoria.cs
+++ CamadadeApresentacao/frmCategoria.cs
@@ -12,8 +12,58 @@ namespace CamadadeApresentacao
         {
             InitializeComponent();
             this.ttMensagem.SetToolTip(this.txtNome, "Insira o nome da Categoria");
-
         }
+
+        // Mostrar mensagem de confirmação
+        private void MensagemOk(string mensagem)
+        {
+            MessageBox.Show(mensagem, "Sistma Comércio", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        // Mostrar mensagem de erro
+        private void MensagemErro(string mensagem)
+        {
+            MessageBox.Show(mensagem, "Sistma Comércio", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        // Limpar campos
+        private void Limpar()
+        {
+            this.txtNome.Text = string.Empty;
+            this.txtIdCategoria.Text = string.Empty;
+            this.txtDescricao.Text = string.Empty;
+        }
+
+        //Habilitar os text box
+        private void Habilitar(bool valor)
+        {
+            this.txtNome.ReadOnly = !valor;
+            this.txtDescricao.ReadOnly = !valor;
+            this.txtIdCategoria.ReadOnly = !valor;
+        }
+
+        //Habilitar os botões
+        private void botoes()
+        {
+            if(this.eNovo || this.eEditar)
+            {
+                this.Habilitar(true);
+                this.btnNovo.Enabled = false;
+                this.btnSalvar.Enabled = true;
+                this.btnEditar.Enabled = false;
+                this.btnCancelar.Enabled = true;
+            }else
+            {
+                this.Habilitar(false);
+                this.btnNovo.Enabled = true;
+                this.btnSalvar.Enabled = false;
+                this.btnEditar.Enabled = true;
+                this.btnCancelar.Enabled = false;
+            }
+        }
+
+        //Ocultar as Colunas do Grid
+
 
         private void frmCategoria_Load(object sender, EventArgs e)
         {
